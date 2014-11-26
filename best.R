6a586bf3779b0c65d7302efb14cc0059b5c32c6f
athenasfire <kelsey.miglioretto@gmail.com>
2014-11-25 19:21:07 -0800

    Adding best.R and .gitignore

---
+++ best.R
@@ -0,0 +1,10 @@
+best <- function(state, outcome) {
+	##Read outcome data
+
+	##Check that state and outcome are valid
+
+	##Return the hospital name in that state with the lowest 30-day death 
+	## rate
+
+
+}
